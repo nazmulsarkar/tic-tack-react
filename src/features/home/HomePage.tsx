ef38544b34e7a243c961519c7a53aa1bdaf564b2
Nazmul Hassan <nazmul.phpdroidx@gmail.com>
2021-07-13 16:12:14 +0600

    added a simple home page

---
+++ src/features/home/HomePage.tsx
@@ -0,0 +1,23 @@
+import { observer } from 'mobx-react-lite';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Header, Segment, Image, Button } from 'semantic-ui-react';
+
+export default observer(function HomePage() {
+    return (
+        <Segment inverted textAlign='center' vertical className='masthead'>
+            <Container text>
+                <Header as='h1' inverted>
+                    <Image size='massive' src='/assets/logo.png' alt='logo' style={{ marginBottom: 12 }} />
+                    Tic Tack Toe
+                </Header>
+                    <>
+                        <Header as='h2' inverted content='Welcome to Tic Tacks' />
+                        <Button as={Link} to='/play' size='huge' inverted>
+                            Go to Play!
+                        </Button>
+                    </>
+            </Container>
+        </Segment>
+    )
+})
